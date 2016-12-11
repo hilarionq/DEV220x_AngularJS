4abd1165df774b34b58ec8e50c623dab4ac52a67
Hilarion Eduardo <hilarionq@gmail.com>
2016-12-10 23:59:39 -0400

    Adding binding to the input controls and reset method

--- Mod5Lab/scripts/labController.js
+++ Mod5Lab/scripts/labController.js
@@ -1,5 +1,12 @@
 app.controller('labController', [
     '$scope',
     function ($scope) {
+		    $scope.reset = reset;
+
+        reset();
+
+        function reset(){
+			       $scope.model = {};
+		    }
     }
 ]);
